0a2fef2d28d03aaf561f907df8a1ab19da84edae
Long Vu <hoanglongvu233@gmail.com>
2024-06-25 23:19:35 +0700

    write CRUD API for User model

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -22,9 +24,8 @@ app.use(express.json()); // it's body-parser
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to hompage");
-});
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running");
